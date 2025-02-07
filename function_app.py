badb68934293ac08ab4cd962c503ac1ec42f94f0
nag763 <loic.labeye@tutanota.de>
2025-02-07 17:17:54 +0100

    Parsing the RSS feeds

--- function_app.py
+++ function_app.py
@@ -2,6 +2,10 @@ import azure.functions as func
 import datetime
 import json
 import logging
+from dotenv import load_dotenv 
+import os
+import feedparser
+from datetime import datetime, timedelta
 
 app = func.FunctionApp()
 
@@ -9,7 +13,38 @@ app = func.FunctionApp()
               use_monitor=False) 
 def NewsAggregator(myTimer: func.TimerRequest) -> None:
     
-    if myTimer.past_due:
-        logging.info('The timer is past due!')
-
-    logging.info('Python timer trigger function executed.')
+    load_dotenv()
+    
+    # Stores the previous day number
+    yesterday_date = datetime.now() - timedelta(1)
+    
+    news_stack = []
+    
+    rss_feeds : str = os.getenv("RSS_FEEDS")
+    api_key : str = os.getenv("API_KEY")
+    
+    if not rss_feeds:
+        logging.error("No RSS feed defined, exiting")
+        return -1
+    
+    rss_feeds = rss_feeds.split(',')
+    
+    
+    for feed in rss_feeds:
+        if feed:
+            logging.info(f"Processing feed {feed}")
+            parsed_feed = feedparser.parse(feed)
+            for entry in parsed_feed.entries:
+                if entry.published_parsed.tm_mday == yesterday_date:
+                    news_stack.append({
+                        "title": entry.title,
+                        "link": entry.link,
+                        "summary": entry.summary,
+                        "published": entry.published
+                    })     
+            
+    if not news_stack:
+        logging.warning("No news found yesterday, so bad!")
+    else:
+        logging.info(f"Found {len(news_stack)} news articles")
+    return 0
