8c614cafb95039cda57991c29430fe6727091c26
nag763 <loic.labeye@tutanota.de>
2025-02-07 19:13:13 +0100

    Completing the function

--- function_app.py
+++ function_app.py
@@ -7,6 +7,7 @@ import os
 import feedparser
 from datetime import datetime, timedelta
 from openai import OpenAI
+from azure.communication.email import EmailClient
 
 openai_plot = """
 The main purpose is to aggregate in a readable way the news content that  will be provided
@@ -33,20 +34,50 @@ Don't add extra tags in the response, I just want the content to be in HTML as i
 
 app = func.FunctionApp()
 
+def send_a_mail(sender_mail: str, receiver_mail: str, mail_server: str, subject:str, content: str):
+    try:
+        client = EmailClient.from_connection_string(mail_server)
+
+        message = {
+            "senderAddress": sender_mail,
+            "recipients": {
+                "to": [{"address": receiver_mail}]
+            },
+            "content": {
+                "subject": subject,
+                "html": content
+            },
+            
+        }
+
+        poller = client.begin_send(message)
+        result = poller.result()
+        logging.info("Message sent: ", result)
+
+    except Exception as ex:
+        logging.error(f"An error happened while sending the mail {ex}")
+
+
+
 @app.timer_trigger(schedule="0 0 6 * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
 def NewsAggregator(myTimer: func.TimerRequest) -> None:
 
     load_dotenv()
 
+    curr_date = datetime.now()
+
     # Stores the previous day number
-    yesterday_date = datetime.now() - timedelta(1)
+    yesterday_date = curr_date - timedelta(1)
     yesterday_date_number = yesterday_date.day
 
     news_stack = []
 
     rss_feeds : str = os.getenv("RSS_FEEDS")
     api_key : str = os.getenv("API_KEY")
+    sender_mail : str = os.getenv("MAIL_FROM")
+    receiver_mail : str = os.getenv("MAIL_TO")
+    mail_server : str = os.getenv("MAIL_SERVER")
 
     if not rss_feeds:
         logging.error("No RSS feed defined, exiting")
@@ -55,6 +86,7 @@ def NewsAggregator(myTimer: func.TimerRequest) -> None:
     if not api_key:
         logging.error("No API key defined, exiting")
         return -1
+    
     
     client = OpenAI(api_key=api_key)
 
@@ -77,14 +109,18 @@ def NewsAggregator(myTimer: func.TimerRequest) -> None:
 
     if not news_stack:
         logging.warning("No news found yesterday, so bad!")
-    else:
-        logging.info(f"Found {len(news_stack)} news articles")
-        response = client.chat.completions.create(model="gpt-4o-mini",
-        messages=[
-            {"role": "system", "content": openai_plot},
-            {"role": "user", "content": json.dumps(news_stack)},
-        ])
-        print(response.choices[0].message.content)
-        
+        send_a_mail(sender_mail, receiver_mail, mail_server, subject=f"Today's {curr_date.day}/{curr_date.month} news (nothing)",content="Seems like there is nothing to report today")
+        return
+    
+    logging.info(f"Found {len(news_stack)} news articles")
+    response = client.chat.completions.create(model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": openai_plot},
+        {"role": "user", "content": json.dumps(news_stack)},
+    ])
+    mail_content = response.choices[0].message.content
+    send_a_mail(sender_mail, receiver_mail, mail_server, subject=f"Today's {curr_date.day}/{curr_date.month} news", content=mail_content)
+    logging.info("RSS fetched, mail sent, exiting...")    
+    
 if __name__ == '__main__':
     NewsAggregator(None)
