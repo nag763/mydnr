b147f8e83cf81041ff104929ef47aa5ec7cec69d
nag763 <loic.labeye@tutanota.de>
2025-02-18 16:06:26 +0100

    Catch in errors

--- function_app.py
+++ function_app.py
@@ -7,6 +7,7 @@ import base64
 from datetime import datetime, timedelta
 from openai import OpenAI
 from azure.communication.email import EmailClient
+import traceback
 
 from settings import Settings
 
@@ -48,81 +49,94 @@ def send_a_mail(
     schedule="0 0 6 * * *", arg_name="myTimer", run_on_startup=False, use_monitor=False
 )
 def NewsAggregator(myTimer: func.TimerRequest) -> None:
-    settings = Settings()
+    try:
+        settings = Settings()
 
-    curr_date = datetime.now()
+        curr_date = datetime.now()
 
-    # Stores the previous day number
-    yesterday_date = curr_date - timedelta(1)
-    yesterday_date_number = yesterday_date.day
+        # Stores the previous day number
+        yesterday_date = curr_date - timedelta(1)
+        yesterday_date_number = yesterday_date.day
 
-    news_stack = []
+        news_stack = []
 
-    if not settings.rss_feeds:
-        logging.error("No RSS feed defined, exiting")
-        return -1
+        if not settings.rss_feeds:
+            logging.error("No RSS feed defined, exiting")
+            return -1
 
-    if not settings.api_key:
-        logging.error("No API key defined, exiting")
-        return -1
+        if not settings.api_key:
+            logging.error("No API key defined, exiting")
+            return -1
 
-    rss_feeds = settings.rss_feeds.split(",")
+        rss_feeds = settings.rss_feeds.split(",")
 
-    logging.debug(f"RSS feeds: {rss_feeds}")
+        logging.debug(f"RSS feeds: {rss_feeds}")
 
-    root_function_url = f"{settings.function_url}?code={settings.function_key}&payload="
+        root_function_url = (
+            f"{settings.function_url}?code={settings.function_key}&payload="
+        )
 
-    for feed in rss_feeds:
-        if feed:
-            logging.info(f"Processing feed {feed}")
-            parsed_feed = feedparser.parse(feed)
-            for entry in parsed_feed.entries:
-                if entry.published_parsed.tm_mday == yesterday_date_number:
-                    news_stack.append(
-                        {
-                            "title": entry.title,
-                            "link": entry.link,
-                            "summary": entry.summary,
-                            "published": entry.published,
-                            "recap_link": f"{root_function_url}{base64.urlsafe_b64encode(
-                                json.dumps({"feed": feed, "link": entry.link}).encode()
-                            ).decode()}",
-                        }
-                    )
+        for feed in rss_feeds:
+            if feed:
+                logging.info(f"Processing feed {feed}")
+                parsed_feed = feedparser.parse(feed)
+                for entry in parsed_feed.entries:
+                    if entry.published_parsed.tm_mday == yesterday_date_number:
+                        news_stack.append(
+                            {
+                                "title": entry.title,
+                                "link": entry.link,
+                                "summary": entry.summary,
+                                "published": entry.published,
+                                "recap_link": f"{root_function_url}{base64.urlsafe_b64encode(
+                                    json.dumps({"feed": feed, "link": entry.link}).encode()
+                                ).decode()}",
+                            }
+                        )
 
-    if not news_stack:
-        logging.warning("No news found yesterday, so bad!")
+        if not news_stack:
+            logging.warning("No news found yesterday, so bad!")
+            send_a_mail(
+                settings.sender_mail,
+                settings.receiver_mail,
+                settings.mail_server,
+                subject=f"Today's {curr_date.day}/{curr_date.month} news (nothing)",
+                content="Seems like there is nothing to report today",
+            )
+            return
+
+        logging.info(f"Found {len(news_stack)} news articles")
+
+        chatgpt_user_content = json.dumps(news_stack)
+
+        openai_response = call_chat_gpt_4o_mini(
+            settings.api_key,
+            system_role=settings.openai_plot_for_rss_recap,
+            user_content=chatgpt_user_content,
+        )
+
+        openai_response_parsed = json.loads(openai_response)
+
+        logging.info(f"Response received from OpenAI : {openai_response_parsed}")
+
         send_a_mail(
             settings.sender_mail,
             settings.receiver_mail,
             settings.mail_server,
-            subject=f"Today's {curr_date.day}/{curr_date.month} news (nothing)",
-            content="Seems like there is nothing to report today",
+            subject=f"Today's {curr_date.day:02d}/{curr_date.month:02d} tech insights : {openai_response_parsed["mailTitle"]}",
+            content=openai_response_parsed["mailContent"],
         )
-        return
-
-    logging.info(f"Found {len(news_stack)} news articles")
-
-    chatgpt_user_content = json.dumps(news_stack)
-
-    openai_response = call_chat_gpt_4o_mini(
-        settings.api_key,
-        system_role=settings.openai_plot_for_rss_recap,
-        user_content=chatgpt_user_content,
-    )
-
-    openai_response_parsed = json.loads(openai_response)
-
-    logging.info(f"Response received from OpenAI : {openai_response_parsed}")
-
-    send_a_mail(
-        settings.sender_mail,
-        settings.receiver_mail,
-        settings.mail_server,
-        subject=f"Today's {curr_date.day:02d}/{curr_date.month:02d} tech insights : {openai_response_parsed["mailTitle"]}",
-        content=openai_response_parsed["mailContent"],
-    )
-    logging.info("RSS fetched, mail sent, exiting...")
+        logging.info("RSS fetched, mail sent, exiting...")
+    except Exception as e:
+        logging.error("An error has been met while aggregating the news", e)
+        send_a_mail(
+            settings.sender_mail,
+            settings.receiver_mail,
+            settings.mail_server,
+            subject="Error met while attempting to fetch today news",
+            content=traceback.format_exc(),
+        )
+        return 1
 
 
 @app.route(route="NewsRecap", auth_level=func.AuthLevel.FUNCTION, methods=["GET"])
@@ -147,11 +161,15 @@ def NewsRecap(req: func.HttpRequest) -> func.HttpResponse:
                     system_role=settings.openai_plot_for_article_recap,
                     user_content=entry.content[0].value,
                 )
-                return func.HttpResponse(openai_response, status_code=200, mimetype="text/html")
+                return func.HttpResponse(
+                    openai_response, status_code=200, mimetype="text/html"
+                )
             except Exception as e:
                 logging.error(f"Error while reaching OpenAI : {e}")
-                return func.HttpResponse("Error while processing article", status_code=500)
-            
+                return func.HttpResponse(
+                    "Error while processing article", status_code=500
+                )
+
     return func.HttpResponse("The article wasn't found", status_code=204)
 
 
